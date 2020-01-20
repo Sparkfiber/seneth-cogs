51e91a6cf65a40324135b41278ba357ebd85f9f9
Seneth <eadesjames@me.com>
2020-01-20 10:58:36 -0500

    Tabs/Spaces

--- specialroles/specialroles.py
+++ specialroles/specialroles.py
@@ -6,24 +6,24 @@ import asyncio
 
 
 class SpecialRoles(commands.Cog):
-    """Allow anyone with a certain role to give a specific role"""
+	"""Allow anyone with a certain role to give a specific role"""
 
-    def __init__(self):
-        self.bot = bot
+	def __init__(self):
+		self.bot = bot
 
-    @staticmethod
-    def success(description):
-        embed = Embed(color=0x2ECC71, title="✅ Success", description=description)
-        return embed
+	@staticmethod
+	def success(description):
+		embed = Embed(color=0x2ECC71, title="✅ Success", description=description)
+		return embed
 
-    @staticmethod
-    def notice(description):
-        embed = Embed(color=0xE67E22, title="❕ Notice", description=description)
-        return embed
+	@staticmethod
+	def notice(description):
+		embed = Embed(color=0xE67E22, title="❕ Notice", description=description)
+		return embed
 
-    @staticmethod
-    def error(description):
-        embed = Embed(color=0xE74C3C, title="⚠ Error", description=description)
+	@staticmethod
+	def error(description):
+		embed = Embed(color=0xE74C3C, title="⚠ Error", description=description)
 
 	@commands.Cog.listener()
 	async def on_reaction_add(self, reaction, user):
