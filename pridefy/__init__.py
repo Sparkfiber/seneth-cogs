a7500f405c271c2ca413c6c1bb4533bde526c67e
Seneth <eadesjames@me.com>
2020-01-24 16:14:27 -0500

    Adding economytrickle to test and use use

--- pridefy/__init__.py
+++
@@ -1,6 +0,0 @@
-from redbot.core import data_manager
-from .pridefy import Pridefy
-
-def setup(bot):
-    data_manager.load_bundled_data(cog, __file__)
-    bot.add_cog(Pridefy(bot))
--- specialroles/__init__.py
+++
@@ -1,4 +0,0 @@
-from .specialroles import SpecialRoles
-
-def setup(bot):
-    bot.add_cog(SpecialRoles(bot))
